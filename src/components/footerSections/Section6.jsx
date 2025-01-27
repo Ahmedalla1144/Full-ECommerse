b72da6ada37279442a2910b12030a053d4b595d1
Ahmed alaam <ahmed.alla56756@gmail.com>
2025-01-27 14:53:39 +0200

    edting QRcode

--- src/components/footerSections/Section6.jsx
+++ src/components/footerSections/Section6.jsx
@@ -2,7 +2,7 @@ export default function Section5() {
   return (
     <>
       <section className="text-gray-500" >
-        <p className="text-navba">&copy; Copyright Ahmed {new Date().getFullYear()}-{new Date().getMonth()+1}-{new Date().getDay()+22}. All right reserved</p>
+        <p className="text-navba">&copy; Copyright Ahmed {new Date().getFullYear()}-{new Date().getMonth()+1}-{new Date().getDate()}. All right reserved</p>
       </section>
     </>
   );
